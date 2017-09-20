1a38290bcd060f4db0f63bd8934edd5934c7e8e7
cadenzah <cadenzah93@gmail.com>
2017-09-20 13:59:06 +0900

    Update JSParcer.js
    PhantomJS를 이용하여 크롤링하고 jQuery로 파싱 (수정중)

--- JSParcer.js
+++ JSParcer.js
@@ -1,42 +1,30 @@
+var page = require('webpage').create();
+var url = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EC%A0%95%EB%AA%A8";
+var jqueryUrl = "http://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js";
 
-/*
-$(".lst_relate > ul > li").each(function() {
-            var data = $(this);
-            var content = document.getElementById("result");
-            content = content + data.text() + "(" + data.attr("href") + ")" + "<br>";
-            console.log(content);
+page.onConsoleMessage = function(msg, lineNum, sourceId) {
+    console.log('CONSOLE: ' + msg);
+};
 
-*/
+page.open(url, function(status) {
+    if (status === "success") {
+        page.includeJs(jqueryUrl, function() {
+            page.evaluate(function() {
+                // lastest version on the web
+                console.log("Parcing from related search keyword result : ");
+                
+                // #nx_related_keywords > dl > dd.lst_relate > ul
+                var related_data = $("dd.lst_relate ul");
+                console.log(related_data.text());
 
-var request = require('request'),
-    cheerio = require('cheerio');
-
-var url = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=정모";
-
-//var url = "https://dobest.io/";
-
-request(url, function(error, response, body) {  
-  if (error) throw error;
- //   console.log(body);
-
-    var $ = cheerio.load(body);
-    console.log($);
-/*
-    var postElements = $(".lst_relate > ul > li");
-    postElements.each(function() {
-        var postTitle = $(this).text();
-        var postUrl = $(this).attr("href");
-
-        console.log(postTitle + postUrl + " ");
-
-  });
-
-*/
+                // #nxfr_htp > div.realtime_srch._aside_news_tab > ol:nth-child(2) > li:nth-child(1) > a > span > span
+                var realtime_new_topic = $("div.realtime_srch._aside_news_tab > ol > li > a");
+                console.log(realtime_new_topic.text());
+                
+            });
+            phantom.exit(0);
+        });
+    } else {
+      phantom.exit(1);
+    }
 });
-
-/*
-    var postElements = $("section.posts article.post");
-    postElements.each(function() {
-        var postTitle = $(this).find("h1").text();
-        var postUrl = $(this).find("h1 a").attr("href");
-*/
