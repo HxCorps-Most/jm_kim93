174e1a6f3e30cacbc595a94247d49fdda8946287
cadenzah <cadenzah93@gmail.com>
2018-01-08 16:32:56 +0900

    Add basic function code

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -4,7 +4,16 @@ public class Palindrome
 {
     public static bool IsPalindrome(string word)
     {
+        int length = word.Length;
+        int middle = length / 2;
+        if (word[0].ToString().ToLower() != word[length - 1].ToString()) return false;
+        for (int i = 1; i <= length - middle - 1;i++)
+        {
+            if (word[i] != word[length - 1 - i]) return false;
+        }
+        return true;
         throw new NotImplementedException("Waiting to be implemented.");
+
     }
 
     public static void Main(string[] args)
