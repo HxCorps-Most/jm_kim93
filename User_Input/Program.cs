78b2a3f755f249e78655f03ef07fcedfe854e79c
cadenzah <cadenzah93@gmail.com>
2018-01-08 16:42:29 +0900

    Hello World!

---
+++ User_Input/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace User_Input
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
