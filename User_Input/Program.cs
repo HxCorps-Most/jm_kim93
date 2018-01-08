7384135698ce709ea036c6dd0ee46f924478940a
cadenzah <cadenzah93@gmail.com>
2018-01-08 19:26:46 +0900

    Add basic function code

--- User_Input/Program.cs
+++ User_Input/Program.cs
@@ -1,17 +1,36 @@
 ﻿using System;
 
-public class TextInput {}
+public class TextInput // accepts all characters
+{
+    protected string value;
+    public TextInput() {}
+    public virtual void Add(char c) // Adds the given character to the current value
+    {
+        this.value = this.value + c;
+    }
+    public string GetValue() // Returns the current value
+    {
+        return value;
+    }
+} 
 
-public class NumericInput {}
+public class NumericInput : TextInput  // accepts only digits
+{
+    public NumericInput() {}
+    override public void Add(char c)
+    {
+        if (c >= 48 && c < 57) base.Add(c);
+    }
+}
 
 public class UserInput
 {
     public static void Main(string[] args)
     {
-        //TextInput input = new NumericInput();
-        //input.Add('1');
-        //input.Add('a');
-        //input.Add('0');
-        //Console.WriteLine(input.GetValue());
+        TextInput input = new NumericInput();
+        input.Add('1');
+        input.Add('a');
+        input.Add('0');
+        Console.WriteLine(input.GetValue());
     }
 }
