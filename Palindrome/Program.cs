82f5845a079029061b2921ee051365ad86dd3079
cadenzah <cadenzah93@gmail.com>
2018-01-08 20:01:23 +0900

    Bug fixed - Compatible for either capitals or small

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -7,14 +7,13 @@ public class Palindrome
     {
         int length = word.Length;
         int middle = length / 2;
-        if (word[0].ToString().ToLower() != word[length - 1].ToString()) return false;
-        for (int i = 1; i <= length - middle - 1;i++)
+        word = word.ToLower();
+        for (int i = 0; i <= length - middle - 1;i++)
         {
             if (word[i].ToString() != word[length - 1 - i].ToString()) return false;
         }
         return true;
         throw new NotImplementedException("Waiting to be implemented.");
-
     }
 
     public static void Main(string[] args)
